ab3374058d7fa49897480cb69750dc84fe459317
Heshan Yodagama <yodagamaheshan@gmail.com>
2020-02-07 18:43:05 +0530

    Initial Commit

---
+++ Preference in swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Preference in swiftUI
+//
+//  Created by Heshan Yodagama on 2/7/20.
+//  Copyright © 2020 Heshan Yodagama. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
